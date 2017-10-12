825a40d783f13c2831471b7a4586bd270a2726d1
GG Gonzalez <gg.gonzalez@ymail.com>
2017-10-12 17:34:38 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,18 +2,36 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var cardOne;
+
 var cardOne = cards [0];
 cardsInPlay.push(cardOne);
 console.log("User flipped queen");
+
 var cardTwo;
+
 var cardTwo = cards [1];
 cardsInPlay.push(cardTwo);
 console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+
+var cardId;
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+var flipCard = function(cardId) {
+	if (cardsInPlay.length === 2) {
 	console.log("Great!")
 }
 if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
 }
-	else {alert("Sorry, try again.");
+else {alert("Sorry, try again.");
 	}
+	checkForMatch();
+}
+cardsInPlay.push(cards[cardId])
+flipCard(0);
+flipCard(2);
